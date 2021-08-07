403bd6fb87e651cdb12620985593d230f0e7ddbd
JasmineGuy <guyjasmine.c@gmail.com>
2021-08-07 14:09:00 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const quizData =[
+    {
+        question: "How old is Jasmine?",
+        a: "10",
+        b: "22",
+        c: "32",
+        d: "102",
+        correct: "c"
+    },
+    {
+        question: "What is the most used programming language in 2019?",
+        a: "java",
+        b: "C",
+        c:"Python",
+        d: "Javascript",
+        correct: "d"
+    },
+    {
+        question: "Who is the president of the USA?",
+        a: "Joe Biden",
+        b: "Hillary Clinton",
+        c:"Donald Trump",
+        d: "Barack Obama",
+        correct: "a"
+
+    },
+    {
+        question: "What does html stand for?",
+        a: "hypertext markup language",
+        b: "hyperactive markup language",
+        c:"cascading stylesheet",
+        d: "JSON object notation",
+        correct: "a"
+    },
+    {
+        question: "What year was Javascript launched?",
+        a: "1996",
+        b: "2009",
+        c:"2008",
+        d: "None of the above",
+        correct: "d"
+    }
+
+]
