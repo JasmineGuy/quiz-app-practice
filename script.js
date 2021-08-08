ded9da4cc5a80849bb8a951948c10138996c70a5
JasmineGuy <guyjasmine.c@gmail.com>
2021-08-08 09:00:00 -0500

    added reload quiz feature

--- script.js
+++ script.js
@@ -39,6 +39,72 @@ const quizData =[
         c:"2008",
         d: "None of the above",
         correct: "d"
+    },
+
+];
+
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const question = document.getElementById('question');
+const aText = document.getElementById('a-text');
+const bText = document.getElementById('b-text');
+const cText = document.getElementById('c-text');
+const dText = document.getElementById('d-text');
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
+
+    question.innerText = currentQuizData.question;
+    aText.innerText = currentQuizData.a;
+    bText.innerText = currentQuizData.b;
+    cText.innerText = currentQuizData.c;
+    dText.innerText = currentQuizData.d;
+};
+
+function getSelected(){
+    let answer = undefined;
+    answerEls.forEach((answerEl) => {
+        if(answerEl.checked){
+            answer = answerEl.id;
+        }
+    });
+    return answer;
     }
 
-]
+    function deselectAnswers(){
+
+        answerEls.forEach((answerEl) => {
+            answerEl.checked = false;
+            })
+    };
+
+submitBtn.addEventListener('click', () => {
+    //check to see the answer
+    const currentQuizData = quizData[currentQuiz];
+
+    console.log(currentQuizData)
+    console.log(currentQuiz)
+    const answer = getSelected();
+
+    if (answer){
+        if(answer === quizData[currentQuiz].correct){
+            score++;
+        }
+
+        currentQuiz++;
+        if( currentQuiz < quizData.length){
+            loadQuiz();
+        } else {
+            console.log(score)
+            quiz.innerHTML = `<h2>You answered ${score} out of ${quizData.length} correctly!</h2>`
+        }
+    }     
+
+    });
